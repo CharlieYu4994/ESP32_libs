2492024a0533415ba77bef28965a29be4254d678
CharlieYu <charlieyu4994@outlook.com>
2021-04-24 22:43:02 +0800

    Update sk6812.cpp

--- sk6812/sk6812.cpp
+++ sk6812/sk6812.cpp
@@ -1,24 +1,6 @@
 #include "sk6812.h"
 #include "driver/rmt.h"
 
-class sk6812
-{
-private:
-  int _lednum;
-  int _bitnum;
-  rmt_item32_t *_buffer = NULL;
-  rmt_config_t _skconfig;
-
-public:
-  esp_err_t begin(int pin, int num);
-  esp_err_t end();
-  esp_err_t show();
-  uint32_t packcolor(uint8_t r, uint8_t g, uint8_t b);
-  void setLED(int led, uint32_t grb888);
-  void setLED(int led, uint8_t r, uint8_t g, uint8_t b);
-  void clear(void);
-};
-
 /**
  * @brief Start the SK6821 Matrix
  * 
