ac57757ed8ce8b6477905c75b9ddba9080b6d7c0
CharlieYu4994 <charlieyu4994@outlook.com>
2021-04-25 20:58:32 +0800

    加入 License 信息

--- sk6812/sk6812.h
+++ sk6812/sk6812.h
@@ -1,3 +1,22 @@
+/**
+ * Copyright (C) 2021 CharlieYu
+ * 
+ * This file is part of ESP32_libs.
+ * 
+ * ESP32_libs is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * ESP32_libs is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with ESP32_libs.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #ifndef SK6812_H
 #define SK6812_H
 
