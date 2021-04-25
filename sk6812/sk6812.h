9a2a9f692ded277c8b5324db8bd30c957bd5bb26
CharlieYu <charlieyu4994@outlook.com>
2021-04-25 18:30:39 +0800

    Update sk6812.h

--- sk6812/sk6812.h
+++ sk6812/sk6812.h
@@ -9,18 +9,15 @@ private:
   int _lednum;
   int _bitnum;
   rmt_item32_t *_buffer = NULL;
-  rmt_config_t _skconfig;
+  rmt_config_t _config;
   uint32_t packcolor(uint8_t r, uint8_t g, uint8_t b);
 
 public:
-  sk6812();
+  sk6812(int pin, int num);
   ~sk6812();
-  esp_err_t begin(int pin, int num);
-  esp_err_t end();
   esp_err_t show();
-  void setLED(int led, uint32_t rgb888);
-  void setLED(int led, uint8_t r, uint8_t g, uint8_t b);
-  void clear(void);
+  void set(int led, uint32_t rgb888);
+  void set(int led, uint8_t r, uint8_t g, uint8_t b);
 };
 
 #endif
