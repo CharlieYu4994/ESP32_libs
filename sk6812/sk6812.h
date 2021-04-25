da2ada501d92c432f8e607e308841e08563fe0d1
CharlieYu <charlieyu4994@outlook.com>
2021-04-25 17:45:43 +0800

    Update sk6812.h

--- sk6812/sk6812.h
+++ sk6812/sk6812.h
@@ -1,4 +1,5 @@
-#pragma once
+#ifndef SK6812_H
+#define SK6812_H
 
 #include "driver/rmt.h"
 
@@ -9,13 +10,17 @@ private:
   int _bitnum;
   rmt_item32_t *_buffer = NULL;
   rmt_config_t _skconfig;
+  uint32_t packcolor(uint8_t r, uint8_t g, uint8_t b);
 
 public:
+  sk6812();
+  ~sk6812();
   esp_err_t begin(int pin, int num);
   esp_err_t end();
   esp_err_t show();
-  uint32_t packcolor(uint8_t r, uint8_t g, uint8_t b);
-  void setLED(int led, uint32_t grb888);
+  void setLED(int led, uint32_t rgb888);
   void setLED(int led, uint8_t r, uint8_t g, uint8_t b);
   void clear(void);
 };
+
+#endif
