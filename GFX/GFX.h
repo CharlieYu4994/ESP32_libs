238c7c2fa9a2cc0b27228872ddb97530825ce16e
CharlieYu4994 <charlieyu4994@outlook.com>
2022-03-13 07:44:59 +0000

    update

---
+++ GFX/GFX.h
@@ -0,0 +1,10 @@
+/**
+ * Copyright (c) 2022 CharlieYu4994
+ * 
+ * This software is released under the MIT License.
+ * https://opensource.org/licenses/MIT
+ */
+
+#pragma once
+#include "framebuffers/framebuffer.h"
+
